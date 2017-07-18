062ee751cf0d6f626c5fecb5659438af32d1907f
alexgaoyh <alexgaoyh@sina.com>
2017-07-18 14:38:35 +0800

    branch

--- src/main/java/com/alexgaoyh/zz/base/sysman/controller/SysmanUserController.java
+++ src/main/java/com/alexgaoyh/zz/base/sysman/controller/SysmanUserController.java
@@ -1,6 +1,10 @@
 package com.alexgaoyh.zz.base.sysman.controller;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alexgaoyh.zz.base.sysman.servive.ISysmanUserService;
 
@@ -9,5 +13,10 @@ public class SysmanUserController {
 	@Resource(name = "sysmanUserService")
 	private ISysmanUserService service;
 
-			
+	@ResponseBody
+	@RequestMapping(value = "/list", produces = "text/html;charset=UTF-8")
+	public String list(HttpServletRequest request, String jsonData) {
+		
+		return null;
+	}
 }
