f8751c36fff71d9e65c50a5f7af2e2d60e15ff99
alexgaoyh <alexgaoyh@sina.com>
2017-07-18 17:47:18 +0800

    合并冲突(优先addToGitIndex,之后commit)

--- src/main/java/com/alexgaoyh/zz/base/sysman/controller/SysmanUserController.java
+++ src/main/java/com/alexgaoyh/zz/base/sysman/controller/SysmanUserController.java
@@ -16,7 +16,7 @@ public class SysmanUserController {
 	@ResponseBody
 	@RequestMapping(value = "/list", produces = "text/html;charset=UTF-8")
 	public String list(HttpServletRequest request, String jsonData) {
-		System.out.println(jsonData);
+		request.getParameterNames();
 		return null;
 	}
 }
