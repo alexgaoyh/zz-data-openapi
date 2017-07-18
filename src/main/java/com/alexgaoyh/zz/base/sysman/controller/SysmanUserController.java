fd574e91fc0f1b568fe0f7178534564a96b5912c
alexgaoyh <alexgaoyh@sina.com>
2017-07-18 18:23:52 +0800

    controller部分代码修改

--- src/main/java/com/alexgaoyh/zz/base/sysman/controller/SysmanUserController.java
+++ src/main/java/com/alexgaoyh/zz/base/sysman/controller/SysmanUserController.java
@@ -3,21 +3,24 @@ package com.alexgaoyh.zz.base.sysman.controller;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alexgaoyh.zz.base.sysman.servive.ISysmanUserService;
 
+@Controller
+@ResponseBody
+@RequestMapping(value = "/sysman/user")
 public class SysmanUserController {
 
 	@Resource(name = "sysmanUserService")
 	private ISysmanUserService service;
 
-	@ResponseBody
 	@RequestMapping(value = "/list", produces = "text/html;charset=UTF-8")
 	public String list(HttpServletRequest request, String jsonData) {
 		System.out.println(jsonData);
-		request.getParameterNames();
 		return null;
 	}
+	
 }
