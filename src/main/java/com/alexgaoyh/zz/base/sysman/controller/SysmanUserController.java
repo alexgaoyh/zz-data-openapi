91dbf28e4a7b475e2e05bfdcee494b869993594e
alexgaoyh <alexgaoyh@sina.com>
2017-07-19 13:22:45 +0800

    controller代码生成器

--- src/main/java/com/alexgaoyh/zz/base/sysman/controller/SysmanUserController.java
+++ src/main/java/com/alexgaoyh/zz/base/sysman/controller/SysmanUserController.java
@@ -1,19 +1,16 @@
 package com.alexgaoyh.zz.base.sysman.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alexgaoyh.zz.base.sysman.dao.auto.entity.SysmanUser;
 import com.alexgaoyh.zz.base.sysman.servive.ISysmanUserService;
-import com.alexgaoyh.zz.vo.sysman.user.SysmanUserVO;
-import com.alibaba.fastjson.JSONObject;
 
-@Controller
-@ResponseBody
-@RequestMapping(value = "/sysman/user")
 public class SysmanUserController {
 
 	@Resource(name = "sysmanUserService")
@@ -22,9 +19,30 @@ public class SysmanUserController {
 	@RequestMapping(value = "/list", produces = "text/html;charset=UTF-8")
 	public String list(HttpServletRequest request, String data) {
 		// data = {"page":{"begin":0,"length":10}}
-		SysmanUserVO sysmanUserVO = JSONObject.parseObject(data, SysmanUserVO.class);
-		System.out.println(sysmanUserVO);
+		Map<Object, Object> map = new HashMap<Object, Object>();
+		service.selectPaginationByMap(map);
 		return null;
 	}
 	
+	@RequestMapping(value = "/save", produces = "text/html;charset=UTF-8")
+	public String save(HttpServletRequest request, String data) {
+		SysmanUser record = new SysmanUser();
+		service.insertSelective(record);
+		return null;
+	}
+	
+	@RequestMapping(value = "/update", produces = "text/html;charset=UTF-8")
+	public String update(HttpServletRequest request, String data) {
+		SysmanUser record = new SysmanUser();
+		service.updateByPrimaryKeySelective(record);
+		return null;
+	}
+	
+	@RequestMapping(value = "/selectone", produces = "text/html;charset=UTF-8")
+	public String selectOne(HttpServletRequest request, String data) {
+		String primaryKey = "";
+		service.selectByPrimaryKey(primaryKey);
+		return null;
+	}
+			
 }
